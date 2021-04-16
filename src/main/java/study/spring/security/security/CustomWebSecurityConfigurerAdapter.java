000d92d23839e48276c9ce57cf73d42fbe9b8416
Oleh <oleg24453@gmail.com>
2021-04-16 21:55:49 +0300

    implemented custom authProvider

---
+++ src/main/java/study/spring/security/security/CustomWebSecurityConfigurerAdapter.java
@@ -0,0 +1,33 @@
+package study.spring.security.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+    private final AuthenticationProvider authenticationProvider;
+
+    public CustomWebSecurityConfigurerAdapter(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/HW/insecure")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+    }
+}
